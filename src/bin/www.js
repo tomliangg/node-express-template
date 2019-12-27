5b3169685edde94a981dfafd06592cbd5e906927
Tom Liang <tliang@xmatters.com>
2019-12-27 09:56:54 -0800

    node express.js app init

---
+++ src/bin/www.js
@@ -0,0 +1,12 @@
+// bin/www.js
+/**
+ * Module dependencies.
+ */
+import app from '../app';
+import debugLib from 'debug';
+const debug = debugLib('your-project-name:server');
+// generated code below.
+
+app.listen(4000, () => {
+    console.log('listening to port 4000 now')
+})
